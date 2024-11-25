4d3ddb960bd7a046fa3d6d6dad89a696fef8c814
Da Silva Clement <clementdasilva0@gmail.com>
2024-11-25 15:14:22 +0100

    réinitialisation de mot de passe (pas réussie a push BDD, problème de XAMPP)

--- src/Fonctions/fonctions.php
+++ src/Fonctions/fonctions.php
@@ -1,17 +1,25 @@
 <?php
 namespace App\Fonctions;
-    function Redirect_Self_URL():void{
-        unset($_REQUEST);
-        header("Location: ".$_SERVER['PHP_SELF']);
-        exit;
-    }
+function Redirect_Self_URL():void{
+    unset($_REQUEST);
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit;
+}
 
 function GenereMDP($nbChar) :string{
 
     return "secret";
 }
 
-function CalculComplexiteMdp($mdp) :int{
-
-
+function passgen1($nbChar)
+{
+//    $chaine = "ABCDEFGHIJKLMONOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&é\"'(-è_çà)=$^*ù!:;,~#{[|`\^@]}¤€";
+    $chaine = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^*";
+    $pass = '';
+    $longueurChaine = strlen($chaine);
+    for ($i = 0; $i < $nbChar; $i++) {
+        $indexAleatoire = random_int(0, $longueurChaine - 1);
+        $pass .= $chaine[$indexAleatoire];
+    }
+    return $pass;
 }
