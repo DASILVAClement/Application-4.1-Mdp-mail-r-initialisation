7b7148abf21eb4cb2c4cd8c2b9757211f5d6c488
Da Silva Clement <clementdasilva0@gmail.com>
2024-12-11 14:21:19 +0100

    réinitialisation du mdp envoie par mail, problème avec changement BDD

--- Controleur/Controleur_visiteur.php
+++ Controleur/Controleur_visiteur.php
@@ -22,9 +22,9 @@ switch ($action) {
                 // Envoyer l'e-mail avec le lien
                 $mail = new PHPMailer();
                 $mail->addAddress($utilisateur["login"]);
-                $mail->Subject = 'Lien réinitialisation de mot de passe';
+                $mail->Subject = 'Réinitialisation de mot de passe';
                 $mail->isHTML(true);
-                $mail->Body = "Voici votre lien : <a href='$lien'>Lien</a>";
+                $mail->Body = "Lien pour réinitialiser <a href='$lien'>Lien à cliquer</a>";
 
                 if ($mail->send()) {
                     $Vue->addToCorps(new Vue_Mail_Confirme());
@@ -39,6 +39,88 @@ switch ($action) {
         }
         break;
 
+    case "Se connecter" :
+        if (isset($_REQUEST["compte"]) and isset($_REQUEST["password"])) {
+            //Si tous les paramètres du formulaire sont bons
+
+            $utilisateur = Modele_Utilisateur::Utilisateur_Select_ParLogin($_REQUEST["compte"]);
+
+            if ($utilisateur != null) {
+                //error_log("utilisateur : " . $utilisateur["idUtilisateur"]);
+                if ($utilisateur["desactiver"] == 0) {
+                    if (isset($_SESSION["motdepasseProv"])) {
+                        if ($_SESSION["motdepasseProv"]==$utilisateur["motDePasse"]) {
+                            $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("", "Gerer_monCompte"));
+                            session_destroy();
+                        }
+                    } else {
+                        if ($_REQUEST["password"] == $utilisateur["motDePasse"]) {
+                            $_SESSION["idUtilisateur"] = $utilisateur["idUtilisateur"];
+                            $_SESSION["idCategorie_utilisateur"] = $utilisateur["idCategorie_utilisateur"];
+
+                            if (Modele_Utilisateur::Utilisateur_Select_RGPD($_SESSION["idUtilisateur"]) == 0) {
+                                include "./Controleur/Controleur_AccepterRGPD.php";
+                                $erreur = true;
+                            } else
+                                switch ($utilisateur["idCategorie_utilisateur"]) {
+                                    case 1:
+                                        $_SESSION["typeConnexionBack"] = "administrateurLogiciel"; //Champ inutile, mais bien pour voir ce qu'il se passe avec des étudiants !
+                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                        break;
+                                    case 2:
+                                        $_SESSION["typeConnexionBack"] = "gestionnaireCatalogue";
+                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                        $Vue->addToCorps(new \App\Vue\Vue_AfficherMessage("Bienvenue " . $_REQUEST["compte"]));
+                                        break;
+                                    case 3:
+                                        $_SESSION["typeConnexionBack"] = "entrepriseCliente";
+                                        //error_log("idUtilisateur : " . $_SESSION["idUtilisateur"]);
+                                        $_SESSION["idEntreprise"] = Modele_Entreprise::Entreprise_Select_Par_IdUtilisateur($_SESSION["idUtilisateur"])["idEntreprise"];
+                                        include "./Controleur/Controleur_Gerer_Entreprise.php";
+                                        break;
+                                    case 4:
+                                        $_SESSION["typeConnexionBack"] = "salarieEntrepriseCliente";
+                                        $_SESSION["idSalarie"] = $utilisateur["idUtilisateur"];
+                                        $_SESSION["idEntreprise"] = Modele_Salarie::Salarie_Select_byId($_SESSION["idUtilisateur"])["idEntreprise"];
+                                        include "./Controleur/Controleur_Catalogue_client.php";
+                                        break;
+                                    case 5:
+                                        $_SESSION["typeConnexionBack"] = "commercialCafe";
+                                        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                                        break;
+                                }
+
+                        } else {//mot de passe pas bon
+                            $msgError = "Mot de passe erroné";
+
+                            $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+
+                        }
+                    }
+                } else {
+                    $msgError = "Compte désactivé";
+
+                    $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+
+                }
+            } else {
+                $msgError = "Identification invalide";
+
+                $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+            }
+        } else {
+            $msgError = "Identification incomplete";
+
+            $Vue->addToCorps(new Vue_Connexion_Formulaire_client($msgError));
+        }
+        break;
+
+    case 'token':
+        $token = rawurldecode($_GET['token']) ?? null;
+        if ($token) {
+            $Vue->addToCorps(new \App\Vue\Vue_Mail_ChoisirNouveauMdp($token));
+            break;
+        }
     default:
         $Vue->addToCorps(new Vue_Connexion_Formulaire_client());
         break;
